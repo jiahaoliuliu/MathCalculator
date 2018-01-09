7a5bb037187d2cace8e4424fa483d746daddeb7c
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2018-01-09 14:11:18 +0800

    Create the basic recycler view

--- app/src/main/java/com/jiahaoliuliu/mathcalculator/MainViewModel.java
+++ app/src/main/java/com/jiahaoliuliu/mathcalculator/MainViewModel.java
@@ -2,7 +2,6 @@ package com.jiahaoliuliu.mathcalculator;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
--- app/src/main/java/com/jiahaoliuliu/mathcalculator/ResultActivity.java
+++ app/src/main/java/com/jiahaoliuliu/mathcalculator/ResultActivity.java
@@ -1,18 +1,31 @@
 package com.jiahaoliuliu.mathcalculator;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.jiahaoliuliu.mathcalculator.databinding.ActivityResultBinding;
+
 public class ResultActivity extends AppCompatActivity {
+
+    private ActivityResultBinding activityResultBinding;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_result);
+        activityResultBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_result);
+
+        activityResultBinding.resultList.hasFixedSize();
+        layoutManager = new LinearLayoutManager(this);
+        activityResultBinding.resultList.setLayoutManager(layoutManager);
     }
 
 }
