98040855b06c25179e90375b4932676103b1d21e
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2018-01-09 13:01:14 +0800

    Set the action for next button

--- app/src/main/java/com/jiahaoliuliu/mathcalculator/CalculateActivity.java
+++ app/src/main/java/com/jiahaoliuliu/mathcalculator/CalculateActivity.java
@@ -1,23 +1,55 @@
 package com.jiahaoliuliu.mathcalculator;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.jiahaoliuliu.mathcalculator.databinding.ActivityCalculateBinding;
 
-public class CalculateActivity extends AppCompatActivity {
+public class CalculateActivity extends AppCompatActivity implements CalculationClickListener {
 
+    private static final String TAG = "CalculateActivity";
+
+    private ActivityCalculateBinding activityCalculateBinding;
     private MainViewModel mainViewModel;
+    private MathOperationModel currentOperationModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityCalculateBinding activityCalculateBinding =
+        activityCalculateBinding =
                 DataBindingUtil.setContentView(this, R.layout.activity_calculate);
+        activityCalculateBinding.setCalculationClickListener(this);
 
         mainViewModel = MainViewModel.getInstance();
-        activityCalculateBinding.setMathOperation(mainViewModel.getNextMathOperationModel());
+        currentOperationModel = mainViewModel.getNextMathOperationModel();
+        activityCalculateBinding.setMathOperationModel(currentOperationModel);
     }
 
+    @Override
+    public void onNextClicked() {
+        // Get the result
+        String givenResultString = activityCalculateBinding.givenResult.getText().toString();
+        if (TextUtils.isEmpty(givenResultString)) {
+            Log.w(TAG, "The input given result is empty");
+            Toast.makeText(this, R.string.error_given_result_empty, Toast.LENGTH_LONG).show();
+            return;
+        }
+        currentOperationModel.setGivenResult(
+                Integer.parseInt(givenResultString));
+        // Show next result
+        Intent showNextCalculateActivityIntent = new Intent(this, CalculateActivity.class);
+        startActivity(showNextCalculateActivityIntent);
+        finish();
+    }
+
+    @Override
+    public void onFinishClicked() {
+        // TODO: Finish the current activity
+        int operation = 1;
+    }
 }
